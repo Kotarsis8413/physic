2da0714d98d23ff703d3585637fa2058afc33434
Кот <kotarsis8413@bk.ru>
2024-01-17 19:25:30 +0300

    update 1.2

--- check.js
+++ check.js
@@ -1,3 +1,8 @@
-if (device.mobile()) {
-    window.location = "/mobile/"
+const userAgent = navigator.userAgent.toLowerCase();
+  const isMobile = /mobile|iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(userAgent);
+
+window.onload = function() {
+    if (isMobile) {
+        window.location = "https://kotarsis8413.github.io/physic/mobile";
+    }
 }
